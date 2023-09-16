b71b6d6e59d97dc8c9febd76cc843a5d1bf8472d
Randy <o1984531@gmail.com>
2023-09-17 02:15:18 +0800

    Add Async & Await sample with API call

---
+++ SwiftPractice.playground/Pages/AsyncAwaitSample1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+struct PostData: Codable {
+    let id: Int
+    let title: String
+}
+
+
+let url = URL(string: "https://my-json-server.typicode.com/Yomiamy/FakeJsonServer/posts")
+
+
+print("Task.init...")
+Task.init {
+    let (data, response) = try await URLSession.shared.data(from: url!)
+    
+    guard let httpResponse = response as? HTTPURLResponse,
+          httpResponse.statusCode == 200 else {
+        print("API failed")
+        fatalError("API failed")
+    }
+    
+    print("httpResponse.status = \(httpResponse.statusCode)")
+    print("data = \(String(data: data, encoding: .utf8)!)")
+    
+    let decoder = JSONDecoder()
+    let encoder = JSONEncoder()
+    
+    do {
+        let postDataAry: [PostData] = try decoder.decode([PostData].self, from: data)
+        print("postDataAry:\n \(postDataAry.map({ data -> String in "\(data.id) : \(data.title)"}).joined(separator: "\n"))")
+    } catch {
+        print("error = \(error)")
+    }
+}
+
+
