37c9c40a0bead1aac0f7e6927cb2522f9dca0cf3
Randy <o1984531@gmail.com>
2023-09-18 22:40:58 +0800

    Add playground liveView for UIImageView

---
+++ SwiftPractice.playground/Pages/ImageWithOverlap.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+//: [Previous](@previous)
+
+import UIKit
+import PlaygroundSupport
+
+let winnie = UIImage(named: "winnie_2")
+let winnieImageView = UIImageView(image: winnie)
+winnieImageView.frame = CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: 200, height: 200))
+winnieImageView.backgroundColor = UIColor(red: 10/255, green: 186/255, blue: 181/255, alpha: 1)
+winnieImageView.contentMode = .scaleAspectFill
+
+PlaygroundPage.current.liveView = winnieImageView
+
+//: [Next](@next)
