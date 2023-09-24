a3d730feea73d6695d1ef5d9b58cd8e62f0ff97c
Randy <o1984531@gmail.com>
2023-09-25 00:57:52 +0800

    Add Dictionary sample
    Add Dictionary sample

---
+++ SwiftPractice.playground/Pages/DictionarySample.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var dict1 = ["name":"Tom", "author": "Sand Lee"]
+dict1["name"]
+dict1["author"]
+dict1["author2"]
+
+dict1.removeValue(forKey: "name")
+dict1["name"]
+
+// Dictionary 宣告方式
+var dict2 = [String:String]()
+var dict3 = Dictionary<String, String>()
+var dict4 = ["name":"Tom", "author": "Sand Lee"]
+
+
+
