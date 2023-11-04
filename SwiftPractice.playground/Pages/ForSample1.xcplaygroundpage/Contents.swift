2fca5265df6352f0e39ace6503467a5e42205b39
Randy <o1984531@gmail.com>
2023-11-04 20:46:04 +0800

    Add For sample

---
+++ SwiftPractice.playground/Pages/ForSample1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+//: [Previous](@previous)
+
+import Foundation
+
+for i in stride(from: 10, to: 0, by: -1) {
+    print(i)
+}
+
+for i in 0...10 {
+    print(i)
+}
+
+for i in 0..<10 {
+    print(i)
+}
+//: [Next](@next)
