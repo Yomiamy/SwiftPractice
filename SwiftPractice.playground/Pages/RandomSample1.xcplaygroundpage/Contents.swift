e7707184ba3594b08360a731555ef92f439741d3
Randy <o1984531@gmail.com>
2023-11-04 20:46:19 +0800

    Add RandomSample1

---
+++ SwiftPractice.playground/Pages/RandomSample1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+//: [Previous](@previous)
+
+import Foundation
+
+
+var number1 = Int.random(in: 0...2)
+print("random number = \(number1)")
+
+var number2 = Double.random(in: 0...2)
+print("random number = \(number2)")
+
+//: [Next](@next)
