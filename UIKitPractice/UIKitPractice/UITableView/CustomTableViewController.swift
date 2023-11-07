e4586dc0f09434557555d665f17bacfe1b40f113
Randy <o1984531@gmail.com>
2023-11-07 23:12:49 +0800

    Add UITableView sample --- TableViewCell height consider by Layout Constraint

--- UIKitPractice/UIKitPractice/UITableView/CustomTableViewController.swift
+++ UIKitPractice/UIKitPractice/UITableView/CustomTableViewController.swift
@@ -13,4 +13,26 @@ class CustomTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func viewDidLayoutSubviews() {
+//        self.tableView.estimatedRowHeight = UITableView.automaticDimension
+//        self.tableView.rowHeight = UITableView.automaticDimension
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        20
+    }
+    
+//    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        100
+//    }
+    
+    // MARK: - DataDelegate
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell") else {
+            fatalError("No cell inited")
+        }
+        
+        return cell
+    }
 }
